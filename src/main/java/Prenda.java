8fdab83b906e95f017b12206654c8b80392c67e2
Rafael Olmos <AlexanderOlmos1997@gmail.com>
2022-04-29 09:06:07 -0300

    Arreglo de QMP1

--- src/main/java/Prenda.java
+++ src/main/java/Prenda.java
@@ -1,6 +1,10 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 public class Prenda {
+    private static Map<CategoriaPrenda, List<TipoDePrenda>> prendaPosible = new HashMap<CategoriaPrenda, List<TipoDePrenda>>();
     CategoriaPrenda categoriaPrenda;
     TipoDePrenda tipoDePrenda;
     Material material;
@@ -11,10 +15,16 @@ public class Prenda {
                   Material material,
                   Color principal,
                   Color secundario) throws PrendaImposibleException {
-        ReglasModa.verificarCreacion(categoriaPrenda,tipoDePrenda);
+        verificarCreacion(categoriaPrenda,tipoDePrenda);
         this.categoriaPrenda = Objects.
                                requireNonNull(categoriaPrenda,"ERROR: Indique categoria de prenda");
         //Asi con los demas hasta principal
         this.secundario = secundario;
+    }
+
+    public static void verificarCreacion(CategoriaPrenda categoriaPrenda, TipoDePrenda tipoDePrenda) throws PrendaImposibleException {
+        if(!prendaPosible.get(categoriaPrenda).contains(tipoDePrenda)){
+            throw new PrendaImposibleException("ERROR: Categoria no coencide con su tipo de prenda");
+        }
     }
 }
